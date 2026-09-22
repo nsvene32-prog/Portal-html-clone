1fa0bba597981656646409ecb2808b68ab67f5d6
nsvene32-prog <nsvene32@students.shrewsbury.k12.ma.us>
2026-09-22 13:10:39 -0400

    Add imports for THREE.js and loaders

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/MTLLoader.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, 
